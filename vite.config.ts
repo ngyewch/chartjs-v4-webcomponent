a5c4c4d6cf8be6f68937fe6e9aa34896959a8c6c
Nick Ng <ngyewch@gmail.com>
2024-09-15 11:15:21 +0800

    chore: updated dist files

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,19 @@
 import {defineConfig} from 'vite';
 import dts from 'vite-plugin-dts';
 
-export default defineConfig(() => {
-    return {
-        build: {
-            minify: true,
-            sourcemap: true,
-            lib: {
-                entry: './src/index.ts',
-                name: 'chartjs-v4-webcomponent',
-                fileName: (format) => `chartjs-v4-webcomponent.${format}.js`,
-            },
+export default defineConfig({
+    build: {
+        minify: true,
+        sourcemap: true,
+        lib: {
+            entry: './src/index.ts',
+            name: 'chartjs-v4-webcomponent',
+            formats: ['es'],
         },
-        plugins: [
-            dts({
-                rollupTypes: true,
-            }),
-        ],
-    };
+    },
+    plugins: [
+        dts({
+            rollupTypes: true,
+        }),
+    ],
 });
